513cc43fbab9972977e37eaaedf12aa445a6b146
Aditya Rahman <adit.adityarahman453@gmail.com>
2022-12-17 00:07:40 +0700

    first commit

---
+++ application/views/pesanan_saya.php
@@ -0,0 +1,27 @@
+<div class="container">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">No Order</th>
+      <th scope="col">Status Pembayaram</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php foreach ($pesanan as $key => $pesanan) { ?>
+        <tr>
+               <th scope="row">1</th>
+               <td><?=$pesanan->no_order?></td>
+               <?php if($pesanan->status_code == 201){?>
+               <td>Pending</td>
+               <?php } else { ?>
+                <td>Sukses</td>
+               <?php }?>
+            </tr>
+      <?php  } ?>
+   
+    
+  </tbody>
+</table>
+    <!-- <a id="pay-button" href="" class="btn btn-primary mt-3">Bayar</a> -->
+</div>
